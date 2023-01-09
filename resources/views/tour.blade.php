eec64fd7cc5105b0eb8e7d487aef358730819b1d
guicrispa <guicrispim1000@gmail.com>
2023-01-09 11:12:08 -0300

    saving

--- resources/views/tour.blade.php
+++ resources/views/tour.blade.php
@@ -20,7 +20,7 @@
             <!-- <iframe src="https://www.google.com/maps/@-23.9369261,-46.3219861,3a,75y,251.37h,62.1t/data=!3m6!1e1!3m4!1sAF1QipO-49G0q0-61WuB2NaHlb17WGvDt8Mfpu-U-m8!2e10!7i7220!8i3610" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe> -->
                 <!--<iframe width="600" height="450" frameborder="0" style="border:0" allowfullscreen="" data-src="https://www.google.com/maps/@-23.9369261,-46.3219861,3a,75y,251.37h,62.1t/data=!3m7!1e1!3m5!1sAF1QipO-49G0q0-61WuB2NaHlb17WGvDt8Mfpu-U-m8!2e10!3e12!7i7220!8i3610" class=" lazyloaded" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="></iframe> --->
 
-                <iframe src="https://www.google.com/maps/embed?pb=!4v1672066306128!6m8!1m7!1sCAoSK0FGMVFpcE8tNDlHMHEwLTYxV3VCMk5hSGxiMTdXR3ZEdDhNZnB1LVUtbTg.!2m2!1d-23.93692612622863!2d-46.32198605686426!3f220!4f0!5f0.7820865974627469" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe title="Tour Bistec" src="https://www.google.com/maps/embed?pb=!4v1672066306128!6m8!1m7!1sCAoSK0FGMVFpcE8tNDlHMHEwLTYxV3VCMk5hSGxiMTdXR3ZEdDhNZnB1LVUtbTg.!2m2!1d-23.93692612622863!2d-46.32198605686426!3f220!4f0!5f0.7820865974627469" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </p>
         </section>
     </article>
